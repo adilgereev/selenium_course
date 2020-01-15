e8e179bf73ca465ade2b4e2915495b3b5388955b
adilgereev <57193512+adilgereev@users.noreply.github.com>
2020-01-15 15:09:07 +0300

    lesson2.2_step6

--- lesson2.2_step3.py
+++ lesson2.2_step3.py
@@ -11,14 +11,12 @@ y = browser.find_element_by_css_selector("#num2").text
 
 xy = str(int(x) + int(y))
 
-
 # Находим дроп даун и элемент дропдауна по сумме "xy"
-dropdown1 = browser.find_element_by_tag_name("select").click()
-element = browser.find_element_by_css_selector(f"[value='{xy}']").click()
-clock = browser.find_element_by_css_selector("body > div > form > button").click()
-
+browser.find_element_by_tag_name("select").click()
+browser.find_element_by_css_selector(f"[value='{xy}']").click()
+browser.find_element_by_css_selector("body > div > form > button").click()
 
 # Ожидание чтобы визуально оценить результаты прохождения скрипта
-time.sleep(5)
+time.sleep(15)
 # закрываем браузер после всех манипуляций
 browser.quit()
---
+++ lesson2.2_step6.py
@@ -0,0 +1,36 @@
+from selenium import webdriver
+import time
+import math
+
+link = "http://SunInJuly.github.io/execute_script.html"
+browser = webdriver.Chrome()
+browser.get(link)
+
+
+# Вычисление требуемое на странице link
+def calc(x):
+    return str(math.log(abs(12 * math.sin(int(x)))))
+
+
+x_element = browser.find_element_by_css_selector("#input_value")
+x = x_element.text
+y = calc(x)
+
+# Ввод вычесленного значения в input
+header = browser.find_element_by_tag_name("body > div > form > div.form-group > label > span:nth-child(1)")
+browser.execute_script("return arguments[0].scrollIntoView(true);", header)
+
+input = browser.find_element_by_css_selector("#answer")
+input.send_keys(y)
+checkbox = browser.find_element_by_css_selector("#robotCheckbox")
+checkbox.click()
+radiobutton = browser.find_element_by_css_selector("#robotsRule")
+radiobutton.click()
+
+button = browser.find_element_by_css_selector("body > div > form > button")
+button.click()
+
+# ожидание чтобы визуально оценить результаты прохождения скрипта
+time.sleep(5)
+# закрываем браузер после всех манипуляций
+browser.quit()
